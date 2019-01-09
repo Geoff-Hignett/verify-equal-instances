aff72e9638a7bb36f7098eec963a8cbfac6d82a8
Geoff-Hignett <geoffhig@hotmail.com>
2019-01-09 16:29:35 +0000

    first commit

---
+++ verify equal instances.js
@@ -0,0 +1,12 @@
+// Verify there are equal instances of specified characters in a string
+
+// For example       XO(xoxojjjfjxo) returns true 
+//                   XO(xoo) returns false
+
+function XO(str) {
+  // Replace characters with empty strings
+  let xCount = str.replace(/x/gi, "").length;
+  let oCount = str.replace(/o/gi, "").length;
+  // Compare string lengths 
+  return oCount == xCount;
+}
